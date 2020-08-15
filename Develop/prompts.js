a69163eca683fa4bd4417b06896409062473d917
Alvin <alvinzcoxiv@gmail.com>
2020-08-15 14:07:33 -0400

    Can show all tables by accessing through inquire

--- Develop/prompts.js
+++ Develop/prompts.js
@@ -1,28 +1,50 @@
 const inquirer = require("inquirer")
-
+const cTable = require("console.table")
 
 const initApp = function(connection) {
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "menu",
-            message: "What would you like to do?",
-            choices: [
-                "View Departments",
-                "Exit"
-            ]
-        }
-    ]).then(function(ans) {
-        console.log("Here comes the response")
-        if (ans.menu === "View Departments") {
-            console.log("Response was to view departments")
-            
-            connection.query("SELECT * FROM department", function (err, res) {
-                if (err) throw err
-                console.log(res)
-            })
-        }
-    })
+    const topMenu = () => {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "menu",
+                message: "What would you like to do?",
+                choices: [
+                    "View Departments",
+                    "View Roles",
+                    "View Employees",
+                    "Exit"
+                ]
+            }
+        ]).then(function(ans) {
+            // console.log("Here comes the response")
+            if (ans.menu === "View Departments") {
+                connection.query("SELECT * FROM department", function (err, res) {
+                    if (err) throw err
+                    console.table(res)
+                    topMenu()
+                })
+            }
+            else if (ans.menu === "View Roles") {
+                connection.query("SELECT * FROM role", function (err, res) {
+                    if (err) throw err
+                    console.table(res)
+                    topMenu()
+                })
+            }
+            else if (ans.menu === "View Employees") {
+                connection.query("SELECT * FROM employee", function (err, res) {
+                    if (err) throw err
+                    console.table(res)
+                    topMenu()
+                })
+            }
+            else if (ans.menu === "Exit") {
+                connection.end()
+                return
+            }
+        })
+    }
+    topMenu()
 }
 
 
--- server.js
+++ server.js
@@ -20,34 +20,3 @@ connection.connect(function(err) {
 })
 
 
-// const initApp = function() {
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "menu",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "View Departments",
-//                 "Exit"
-//             ]
-//         }
-//     ]).then(function(ans) {
-//         console.log("Here comes the response")
-//         if (ans.menu === "View Departments") {
-//             console.log("Response was to view departments")
-//             const viewDepartments = () => {
-//                 console.log("Running viewDepartments...")
-//                 connection.query("SELECT * FROM department", function (err, res) {
-//                     if (err) throw err
-//                     console.log(res)
-//                     connection.end
-//                 })
-//             }
-//         runConnect(viewDepartments)
-//         }
-//     })
-// }
-
-
-
-
