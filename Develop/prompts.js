79935f20500905a5c00b97078be1c11afc1a9f87
Alvin <alvinzcoxiv@gmail.com>
2020-08-16 22:26:17 -0400

    Can add roles and employees to their respective tables by using inquirer prompts

--- Develop/prompts.js
+++ Develop/prompts.js
@@ -67,13 +67,78 @@ const initApp = function(connection) {
         ]).then(function(ans) {
             connection.query("INSERT INTO department SET ?", {name: ans.createDept}, function(err, res) {
                 if (err) throw err
+                console.log(res.affectedRows + " departments inserted! \n")
+                topMenu()
+            })
+        })
+    }
+    const addRole = () => {
+        console.log("Add a role")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "roleTitle",
+                message: "What is the title of the role you want to add?"
+            },
+            {
+                type: "input",
+                name: "roleSalary",
+                message: "What will the salary of the new role be?"
+            },
+            {
+                type: "input",
+                name: "roleDept",
+                message: "Enter the Department ID of the department that this role will belong to"
+            }
+        ]).then(function(ans) {
+            connection.query("INSERT INTO role SET ?", {title: ans.roleTitle, salary: ans.roleSalary, department_id: ans.roleDept}, function(err, res) {
+                if (err) throw err
                 console.log(res.affectedRows + " roles inserted! \n")
                 topMenu()
             })
         })
     }
-
-
+    const addEmployee = () => {
+        console.log("Add an employee")
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "firstName",
+                message: "What is the first name of the new employee?"
+            },
+            {
+                type: "input",
+                name: "lastName",
+                message: "What is the last name of the new employee?"
+            },
+            {
+                type: "input",
+                name: "roleId",
+                message: "Please provide the Role ID of the role the new employee will be assigned to"
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please provide the Employee ID of the new employee's manager"
+            }
+        ]).then(function(ans) {
+            if (ans.managerId === "") {
+                ans.managerId = undefined
+            }
+            connection.query("INSERT INTO employee SET ?", {
+                first_name: ans.firstName, 
+                last_name: ans.lastName, 
+                role_id: ans.roleId, 
+                manager_id: ans.managerId
+            }, 
+            function(err, res) {
+                if (err) throw err
+                console.log(res.affectedRows + " employees inserted! \n")
+                topMenu()
+            })
+        })
+    }
+    
     topMenu()
 }
 
